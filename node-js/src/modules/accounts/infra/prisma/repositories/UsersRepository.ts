9409432a7e2b70708c88ec9b3e329faf8d772d1d
Felipe Pichl <felipe.pichl@hotmail.com>
2022-09-27 17:15:07 -0300

    Prisma.IO config - Part II

---
+++ node-js/src/modules/accounts/infra/prisma/repositories/UsersRepository.ts
@@ -0,0 +1,41 @@
+import { ICreateUserDTO } from '@modules/accounts/dtos/ICreateUserDTO';
+import { IUsersRepository } from '@modules/accounts/repositories/IUsersRepository';
+import { PrismaClient } from '@prisma/client';
+
+import { User } from '../entities/User';
+
+class UsersRepository implements IUsersRepository {
+  private prisma: PrismaClient;
+
+  constructor() {
+    this.prisma = new PrismaClient();
+  }
+
+  async create(data: ICreateUserDTO): Promise<void> {
+    await this.prisma.users.create({
+      data,
+    });
+  }
+
+  async findByEmail(email: string): Promise<User> {
+    const reseult = await this.prisma.users.findUnique({
+      where: {
+        email,
+      },
+    });
+
+    return reseult;
+  }
+
+  async findById(user_id: string): Promise<User> {
+    const result = await this.prisma.users.findUnique({
+      where: {
+        id: user_id,
+      },
+    });
+
+    return result;
+  }
+}
+
+export { UsersRepository };
